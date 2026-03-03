46d3cbace889808ea7de669ca55ea7516e755137
Tom LaTulipe <tlatulipe@gmail.com>
2026-03-03 22:49:36 +0000

    suggested change

--- middleware.js
+++ middleware.js
@@ -4,7 +4,8 @@ export default function middleware(req) {
   return next({
     headers: {
       "Referrer-Policy": "origin-when-cross-origin",
-      "X-Frame-Options": "ALLOW",
+      "X-Frame-Options": "SAMEORIGIN",
+      "Content-Security-Policy": "frame-ancestors 'self' https://www.derbynecklibrary.org",
       "X-Content-Type-Options": "nosniff",
       "X-DNS-Prefetch-Control": "on",
       "Strict-Transport-Security":
